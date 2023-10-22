9a54c50e2ff081bf941c0540813a7f4a0396863c
Abdelmalek <93287003+i-Glow@users.noreply.github.com>
2023-10-22 16:04:40 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = {};
-=======
+
 const nextConfig = {
   output: "export",
   basePath: "/portfolio"
 };
->>>>>>> 4c383317c8d624007ca874080e4254bda6725e71
 
 module.exports = nextConfig;
