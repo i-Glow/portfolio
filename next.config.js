fee58abee325073282366eb02a4f1e8b6c386787
Abdelmalek <devmailglow@gmail.com>
2023-12-01 12:15:06 +0100

    apos

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const dev = process.env.NEXT_PUBLIC_DEV === "DEV";
-
-const nextConfig = dev
-  ? {}
-  : {
-      output: "export",
-      basePath: "/portfolio",
-    };
+const nextConfig = {
+  output: "export",
+  basePath: "/portfolio",
+};
 
 module.exports = nextConfig;
