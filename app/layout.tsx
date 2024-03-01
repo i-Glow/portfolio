257e6108cc678b2beff1960c9c67b5786fbb7342
Abdelmalek <devmailglow@gmail.com>
2024-03-01 21:14:20 +0100

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import Script from "next/script";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -16,6 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth bg-slate-900">
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-BD9861Z0M1"
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-BD9861Z0M1');`}
+      </Script>
       <body className={outfit.className}>
         {children}
         <div id="portal"></div>
